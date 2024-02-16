890bbb376c77e76caa9de9d3c92a83de863d6b1b
rafaelswr <rafaels.borges91@gmail.com>
2024-02-16 11:31:10 +0000

    configurar projeto e dependências de inicialização do Spring
    configurar projeto e dependências de inicialização do Spring

---
+++ spring-security-oauth2-keycloak/oauth2-authorization/src/main/java/com/rafaelswr/oauth2authorization/oauth2AuthorizationApplication.java
@@ -0,0 +1,12 @@
+package com.rafaelswr.oauth2authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class oauth2AuthorizationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(oauth2AuthorizationApplication.class, args);
+    }
+
+}
