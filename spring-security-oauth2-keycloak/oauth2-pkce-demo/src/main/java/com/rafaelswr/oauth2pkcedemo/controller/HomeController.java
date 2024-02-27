bb80fa4153c588748667588efeb0ac711b9a9d4b
rafaelswr <rafaels.borges91@gmail.com>
2024-02-27 11:45:22 +0000

    SecurityFilterChain configuration with jwt oauth2 validation from httpRequests
    SecurityFilterChain configuration with jwt oauth2 validation from httpRequests

--- spring-security-oauth2-keycloak/oauth2-pkce-demo/src/main/java/com/rafaelswr/oauth2pkcedemo/controller/HomeController.java
+++ spring-security-oauth2-keycloak/oauth2-pkce-demo/src/main/java/com/rafaelswr/oauth2pkcedemo/controller/HomeController.java
@@ -1,16 +1,15 @@
 package com.rafaelswr.oauth2pkcedemo.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 @CrossOrigin(origins = "*")
 public class HomeController {
 
-    @GetMapping()
+    @GetMapping("/home")
+    @ResponseStatus(HttpStatus.OK)
     public String sayHello(){
         return "Hello";
     }
--- spring-security-oauth2-keycloak/oauth2-pkce-demo/src/main/resources/frontend/src/app/auth.config.ts
+++ spring-security-oauth2-keycloak/oauth2-pkce-demo/src/main/resources/frontend/src/app/auth.config.ts
@@ -1,7 +1,7 @@
 import {AuthConfig} from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
-  issuer:"http://localhost:8180/auth/realms/oauth2-demo-realm",
+  issuer:"http://localhost:8180/realms/oauth2-realm-demo",
   redirectUri:window.location.origin,
   clientId:"oauth2-demo-pkce-client",
   responseType:"code",
